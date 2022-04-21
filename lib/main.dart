81ec76bf3ca6e9c09ae97e81dc2a6664a2344d5a
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-04-20 23:28:45 -0500

    I've introduced some changes to the front of my gallery slider. First, I've removed the greeting placed in the appbar. Second, the images don't fill the entire screen anymore, they are now on top of an institutional red rug and lastly I've replaced the button with a new one with more style.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +31,145 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final SATimages = [
+    'assets/images/SAT-Aguascalientes.png',
+    'assets/images/SAT-CdJuarez.png',
+    'assets/images/SAT-Celaya.png',
+    'assets/images/SAT-Chihuahua.png',
+    'assets/images/SAT-Culiacan.png',
+    'assets/images/SAT-Durango.png',
+    'assets/images/SAT-Hermosillo.png',
+    'assets/images/SAT-Irapuato.png',
+    'assets/images/SAT-Oaxaca.png',
+    'assets/images/SAT-Pachuca.png',
+    'assets/images/SAT-Puebla.png',
+    'assets/images/SAT-Queretaro.png',
+    'assets/images/SAT-Tepic.png',
+    'assets/images/SAT-Veracruz.png',
+    'assets/images/SAT-Zapopan.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    //final double height = MediaQuery.of(context).size.height;
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        leading: Icon(Icons.menu, color: Colors.white),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.75,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image:
+                        AssetImage('assets/images/rounded_backgroundsat.png'),
+                    fit: BoxFit.fill)),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.75,
+            child: CarouselSlider.builder(
+              itemCount: SATimages.length,
+              itemBuilder: (context, index, realIndex) {
+                final SATimage = SATimages[index];
+                return buildImage(SATimage, index);
+              },
+              options: CarouselOptions(
+                //height: 515,
+                height: 300,
+                //viewportFraction: 1,
+                autoPlay: true,
+                enlargeCenterPage: true,
+                //enlargeStrategy: CenterPageEnlargeStrategy.height,
+                //autoPlayInterval: Duration(seconds: 3),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Container(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: 190,
+                height: 45,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xffD9BCA3),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(60),
+                            topRight: Radius.circular(60)),
+                      ),
+                    ),
+                    child: Text(
+                      'Comenzar',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    )),
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          )
+          /*Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                    margin: EdgeInsets.only(bottom: 35),
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Text('Comenzar',
+                                style: TextStyle(fontSize: 18))),
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green[700],
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15))))),
+              ),
+            ],
+          )*/
+          /*Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Container(
+              color: Colors.yellow,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child:
+                              Text('Comenzar', style: TextStyle(fontSize: 18))),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.green[700],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15))))
+                ],
+              ),
+            ),
+          )*/
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget buildImage(String SATimage, int index) => ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 1), //40  //1
+          color: Colors.grey,
+          child: Image.asset(SATimage, fit: BoxFit.cover),
+        ),
+      );
 }
